33a3f07ae7b3868eed454966a92d5b53c1f6c568
Olga Wiederspan <olga.wiederspan@adesso.de>
2022-08-13 13:15:18 +0200

    added DependencyInjection with Spring

---
+++ src/main/java/guru/spring/framework/di/DiApplication.java
@@ -0,0 +1,38 @@
+package guru.spring.framework.di;
+
+import guru.spring.framework.di.controllers.ConstructorInjectedController;
+import guru.spring.framework.di.controllers.MyController;
+import guru.spring.framework.di.controllers.PropertyInjectedController;
+import guru.spring.framework.di.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+
+		System.out.println("----------Property");
+
+		PropertyInjectedController propertyInjectedController=(PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("----------Setter");
+		SetterInjectedController setterInjectedController =(SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("----------Constructor");
+		ConstructorInjectedController constructorInjectedController=(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
