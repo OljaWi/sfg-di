9b3d26be040f9a09097b9fcfe78c38d26f8d095b
Olga Wiederspan <olga.wiederspan@adesso.de>
2022-08-13 14:21:40 +0200

    declared spanish greeting service profile as default profile

--- src/main/java/guru/spring/framework/di/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/spring/framework/di/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.spring.framework.di.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"}) //declaring spanish profile as default profile(if no default profile existing and no profile activated->ERROR)
 @Service("i18nService") //is going to be the name of the Service
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
