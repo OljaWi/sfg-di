870897cd40d620c407ea1029bbf1e5203df6e589
Olga Wiederspan <olga.wiederspan@adesso.de>
2022-08-13 13:51:42 +0200

    added Setter-, Property-, Constructor- and Primary Injected Greeting Service

---
+++ src/main/java/guru/spring/framework/di/services/PrimaryGreetingService.java
@@ -0,0 +1,15 @@
+package guru.spring.framework.di.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World from the PRIMARY Bean!";
+    }
+}
